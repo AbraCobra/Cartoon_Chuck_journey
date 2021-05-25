09b44bff93e930ebbdf33d4ba5a07347505cd073
AbraCobra <iabramova77@mail.ru>
2021-05-25 22:34:01 +0300

    Add some corrections

--- LibIraSDK/Abramova_Irina_EXAMPLE.cpp
+++ LibIraSDK/Abramova_Irina_EXAMPLE.cpp
@@ -1,9 +1,27 @@
+//==========================================================================//
+// DEMO SDK LibIra.h
+//
+// May, 2021
+//=========================================================================//
+
 #include "TXLib.h"
 #include "LibIra.h"
+
+
+void COMMONScene();
 
 int main()
     {
     txCreateWindow (1200, 700);
+
+    COMMONScene();
+
+    return 0;
+    }
+
+void COMMONScene()
+    {
+
     HDC  background_Cartoon = txLoadImage ("BackgroudExample//COMMONPIC.bmp");
 
     int t = 0;
@@ -11,12 +29,13 @@ int main()
     int twitch = 20;
     while (t <= 250)
         {
+
         txBitBlt            (txDC(), 0, 0, 1200, 700, background_Cartoon, 0, 0);
 
         Skateboard_draw     (- 100 + 8*t, 520 - 3*t, 0.8, 0.8);
 
         Skate_wheels_draw   (-  50 + 8*t + ROUND (4*cos(t)) + ROUND (3*sin(t)),
-                            ( 550 - 3*t) + ROUND (3*cos(t)) - ROUND (4*sin(t)), 20);
+                               550 - 3*t + ROUND (3*cos(t)) - ROUND (4*sin(t)), 20);
 
         Chuck_draw          (    0 + 8*t, 420 - 3*t, 0.5, 0.5, (t%2*20) - 10, (t%2*30) + 50);
 
@@ -24,12 +43,12 @@ int main()
 
         Flower_draw         (  150 + 8*t, 380 - 3*t, 0.2, 0.2, RGB (255, 20, 255));
 
-        Butterfly_draw      (  120 + ROUND(15*(t - sin(t))),  50 + ROUND(50*(1 - cos(t))), RGB (100,  39,  51), 2);
+        Butterfly_draw      (  120 + ROUND (15*(t - sin(t))),  50 + ROUND (50*(1 - cos(t))), RGB (100,  39,  51), 2);
 
-        Butterfly_draw      (  100 + ROUND(15*(t - sin(t))), 500 + ROUND(20*(1 - cos(t))), RGB (245, 100,   0), 3);
+        Butterfly_draw      (  100 + ROUND (15*(t - sin(t))), 500 + ROUND (20*(1 - cos(t))), RGB (245, 100,   0), 3);
 
-        Deffy               (  450, 700 - t*3, 0.6, 0.6, abs(t%swing - swing/2) - swing/4, abs(t%twitch - twitch/2) - twitch/4,
-        RGB (217, 0, 217),
+        Deffy               (  450, 700 - t*3, 0.6, 0.6, abs(t%swing - swing/2) - swing/4,
+                               abs(t%twitch - twitch/2) - twitch/4, RGB (217, 0, 217),
                                RGB (56, 160, 56), RGB (130, 66, 151), RGB (55, 183, 193), RGB (242, 238, 77));
 
         Deffy               (  750, 700 - t*3, 0.4, 0.4, abs(t%swing - swing/2) - swing/4, abs(t%twitch - twitch/2) - twitch/4,
@@ -40,6 +59,6 @@ int main()
         }
 
     txDeleteDC (background_Cartoon);
-    return 0;
     }
+
 
